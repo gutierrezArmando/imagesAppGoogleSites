4a0aef379de5709a288215f33a6b2633f7b1b5f6
Armando <37322555+gutierrezArmando@users.noreply.github.com>
2025-10-20 19:58:31 -0700

    Separación secciones principales
    Se separa el sidebar para vista dektop, sidebar para vista movil y el dashboard de gráficas

---
+++ src/app/components/sidebar-movil/sidebar-movil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-movil',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar-movil.component.html',
+  styleUrl: './sidebar-movil.component.css'
+})
+export class SidebarMovilComponent {
+
+}
